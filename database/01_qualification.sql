821c823455c48c2c63c9e3b7f7c131ce6a8338c3
shortf15e <73486744+shortf15e@users.noreply.github.com>
2021-01-04 14:50:41 -0800

    01_quals

---
+++ database/01_qualification.sql
@@ -0,0 +1,21 @@
+CREATE TABLE qualification (
+    `id` int NOT NULL primary key serial,
+    `name` char(10),
+);
+
+INSERT INTO qualification (`name`) VALUES
+    ('MCC'),
+    ('MCCT'),
+    ('RSC'),
+    ('SD'),    
+    ('ST'),
+    ('AWO'),
+    ('WD'),
+    ('ASO'),
+    ('AST'),    
+    ('IDT'),
+    ('TT'),
+    ('ICO'),
+    ('ICOT'),
+    ('ICT')
+ ;
