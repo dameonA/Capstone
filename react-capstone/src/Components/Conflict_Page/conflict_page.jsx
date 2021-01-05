58588e43706cf86c7a4f3af0c741361590f5ae3a
azteckind <73482202+azteckind@users.noreply.github.com>
2021-01-05 10:59:58 -0800

    created conflict page branch with empty conflict page jsx file

---
+++ react-capstone/src/Components/Conflict_Page/conflict_page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+class ConflictPage extends React.Component {
+
+    render() {
+        return (
+            <br />
+        )
+    }
+}//end of ConflictPage-class
+
+export default ConflictPage;
