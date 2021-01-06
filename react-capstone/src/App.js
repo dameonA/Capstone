282cc5deaaea509894bb6c47121f1267be831778
shortf15e <73486744+shortf15e@users.noreply.github.com>
2021-01-05 19:25:13 -0800

    CreateUser
    Also updated App.js and Header.js to include User selection

--- react-capstone/src/App.js
+++ react-capstone/src/App.js
@@ -4,6 +4,7 @@ import {ThemeProvider} from '@material-ui/styles'
 import theme from './Components/ui/Theme'
 import {Route,Switch, BrowserRouter} from 'react-router-dom'
 import Home from './Components/Home/Home'
+import CreateUser from './Components/CreateUser/CreateUser'
 
 
 function App() {
@@ -12,14 +13,18 @@ function App() {
     <ThemeProvider theme={theme}>
       <BrowserRouter>
       <Header/>
+      <div>
         <Switch>
           <Route exact path='/'><Home/></Route>
+          <Route exact path='/Users'><CreateUser/></Route>
           {/* <Route exact path='/mx'><Mx/></Route>
           <Route exact path='/logs'><Logs/></Route>
           <Route exact path='/assetrequest'><AssetRequest/></Route>
           <Route exact path='/opscap'><Opscap/></Route>
           <Route exact path='/posneg'><PosNeg/></Route> */}
         </Switch>
+      </div>
+
       </BrowserRouter>
     </ThemeProvider>
   );
--- react-capstone/src/Components/Home/Home.jsx
+++ react-capstone/src/Components/Home/Home.jsx
@@ -4,7 +4,9 @@ import logo from '../Assets/logo.png'
 export default class Home extends React.Component{
     render(){
         return(
-            <img src={logo} alt="logo" align="center"/>
+            <div>
+                <img src={logo} alt="logo" align="center"/>
+            </div>
         )
     }
 }
