9de1113c012d5dbbdce7876c735c5829be603d5b
dameon <dameon.adolphin@gmail.com>
2021-01-06 07:37:26 -0800

    Consolidating Changes

--- react-capstone/src/Test/ScheduleHomepageTest.jsx
+++ react-capstone/src/Test/ScheduleHomepageTest.jsx
@@ -1,6 +1,41 @@
-import App from '../App';
-import React from 'react';
-import { shallow } from 'enzyme';
+import * as React from 'react'
+import {render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import fetchMock from 'jest-fetch-mock'
+import ConflictPage from '../../Components/Conflict_Page/conflict_page'
+var apiUrl = "MANDO";
 
+beforeEach(() => {
+    fetch.resetMocks();
+});//end of Before Each loop
 
-test
+test('View Entire Schedule exists', async () => {
+    fetch.mockResponse(JSON.stringify({conflict: []}));
+    render(<ConflictPage user={{user_id:2}} api={apiUrl}/>);
+    expect(screen.getByText("Conflict")).toBeInTheDocument();
+    });
+
+test('View Entire Schedule exists', async () => {
+    fetch.mockResponse(JSON.stringify({conflict: []}));
+    render(<ConflictPage user={{user_id:2}} api={apiUrl}/>);
+    expect(screen.getByText("Conflict")).toBeInTheDocument();
+    });
+
+test('View Entire Schedule exists', async () => {
+        fetch.mockResponse(JSON.stringify({conflict: []}));
+        render(<ConflictPage user={{user_id:2}} api={apiUrl}/>);
+        expect(screen.getByText("Conflict")).toBeInTheDocument();
+    });
+
+test('View Entire Schedule exists', async () => {
+    fetch.mockResponse(JSON.stringify({conflict: []}));
+    render(<ConflictPage user={{user_id:2}} api={apiUrl}/>);
+    expect(screen.getByText("Conflict")).toBeInTheDocument();
+    });
+
+test('View Entire Schedule exists', async () => {
+    fetch.mockResponse(JSON.stringify({conflict: []}));
+    render(<ConflictPage user={{user_id:2}} api={apiUrl}/>);
+    expect(screen.getByText("Conflict")).toBeInTheDocument();
+    });//end of conflict page test
+
