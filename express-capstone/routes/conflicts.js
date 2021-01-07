9cc6fe22c42688f58a7cd8c3fd4c9eb93acb4936
azteckind <73482202+azteckind@users.noreply.github.com>
2021-01-07 14:42:45 -0800

    Still working on getting express portion of conflict figured out

--- express-capstone/routes/conflicts.js
+++ express-capstone/routes/conflicts.js
@@ -3,8 +3,14 @@ var express = require("express")
 var ConflictService;
 var router = express.Router()
 
-router.get('', function (request, response) {
+router.get('/conflicts', function (request, response) {
     var conflicts = ConflictService.getConflicts()
+    .then(ret => response.send(ret))
+    .catch(ret => response.send([ ]));
+})
+
+router.post('/conflicts/:id', function (request, response) {
+
 })
 
 module.exports = function(conflictService) {
