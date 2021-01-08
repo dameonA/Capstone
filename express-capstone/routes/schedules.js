e5be4cef2c7a97adc0024e354f9c33413a2ddc4f
dameon <dameon.adolphin@gmail.com>
2021-01-08 13:54:10 -0800

    ExpressChanges and some React configurations

---
+++ express-capstone/routes/schedules.js
@@ -0,0 +1,21 @@
+var express = require('express')
+var ScheduleService;
+var router = express.Router()
+
+router.get('', function (req, res) {
+    var schedule = ScheduleService.getSchedule()
+    .then(ret=>res.send(ret))
+    .catch(ret=>res.send([ ]));
+})
+
+
+module.exports = function(scheduleService){
+  ScheduleService=scheduleService;
+
+
+  // do as you wish
+  // this runs in background, not on each
+  // request
+
+  return router;
+}
