03cafb4bcf37befdd5bf86dcc069e4b675c6ac3e
brandonjohnson21 <brandon.johnson.21@us.af.mil>
2021-01-08 08:22:07 -0800

    remove console.log

--- express-capstone/services/auth.js
+++ express-capstone/services/auth.js
@@ -7,7 +7,6 @@ module.exports.Auth = class Auth {
       try {
           return await this.db.one('SELECT * from user_auth where username = $1',username)
       } catch(error) {
-        console.log(error);
         return undefined;
       }
     }
--- express-capstone/services/users.js
+++ express-capstone/services/users.js
@@ -1,30 +1,3 @@
-const queryUsers = `
-SELECT json_build_object(
-  'user_id', users.user_id, 
-  'first_name',users.first_name,
-  'last_name',users.last_name,
-  'grade',users.grade,
-  'section',users.section,
-  'user_group',users.user_group,
-  'active',users.active,
-  'role', users.user_role, 
-  'qualifications',
-      (SELECT json_agg(
-          json_build_object(
-              'qual_id', user_qualifications.qual_id, 
-              'in_training',user_qualifications.in_training,
-              'is_instructor',user_qualifications.is_instructor,
-              'is_evaluator',user_qualifications.is_evaluator
-            )
-          )
-       FROM user_qualifications WHERE users.user_id = user_qualifications.user_id),
-  'certifications',
-      (SELECT json_agg(json_build_object('cert_id', user_certifications.cert_id))
-       FROM user_certifications WHERE users.user_id = user_certifications.user_id)
-      
-) json
-FROM users
-`
 
 module.exports.Users = class Users {
     constructor(database) {
@@ -32,7 +5,7 @@ module.exports.Users = class Users {
     }
     async getUser (userId) {
       try {
-          return (await this.db.one(queryUsers+' where user_id = $1',userId)).json
+          return await this.db.one('select * from users where user_id = $1',userId)
       } catch(error) {
         //console.log(error);
         return undefined;
@@ -41,7 +14,7 @@ module.exports.Users = class Users {
 
     async getUsers () {
       try {
-          return (await this.db.any(queryUsers)).json
+          return await this.db.any('select * from users')
       } catch(error) {
         //console.log(error);
         return undefined;
