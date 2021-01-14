c5826de60a9d7c2afae63247241b85eca2ab45e9
dameon <dameon.adolphin@gmail.com>
2021-01-14 09:40:13 -0800

    Commit calendar

--- react-capstone/src/Components/Schedules/Pages/ViewEntireSchedule.jsx
+++ react-capstone/src/Components/Schedules/Pages/ViewEntireSchedule.jsx
@@ -1,104 +1,27 @@
-// import React from 'react';
+import React from 'react';
+import SchedulePage from '../children/SchedulePage';
 
+class ViewEntireSchedule extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
 
+    }
+  }
+  render(){
+     return (
+         <div>
+           <div className="row">
+             <div className="col s12">
+                  <h2 className="emp-h2">Schedule Dashboard</h2>
+             </div>
+           </div>
+           <div className="col s12">
+             <SchedulePage />
+           </div>
+         </div>
+     );
+ }
+}
 
-
-// function ViewEntireSchedule() {
-  
-//   const columns = ["First Name", "Last Name", "Position", "Start Time", "End-Time"];
-
-//   const data = [
-//    ["Joe1", "Snuffy", "TT", "2021-01-01 06:30:00 PST", "2021-01-01 18:30:00 PST"],
-//    ["Joe2", "Snuffy", "TT", "2021-01-01 06:30:00 PST", "2021-01-01 18:30:00 PST"],
-//    ["Joe3", "Snuffy", "TT", "2021-01-01 06:30:00 PST", "2021-01-01 18:30:00 PST"],
-//    ["Paul", "Asphalt", "MCC", "2021-01-01 06:30:00 PST'", "2021-01-01 18:30:00 PST"],
-//   ];
-
-//   const options = {
-//       filterType: 'checkbox',
-//     };
-//   return (
-//     <div className="row">
-//                 <header> 
-//                     <h1> Crew Schedule View </h1>
-//                         </header>
-//                 <hr />
-//                 <MUIDataTable 
-//                         data={data} 
-//                         columns={columns} 
-//                         options={options} 
-//                     />
-//             </div>
-//         );
-//     }
-
-// export default ViewEntireSchedule;
-
-
-
-
-// import React from "react";
-// import { Redirect } from 'react-router-dom';
-// import helpers from "./utils/helpers";
-
-// class Employee extends React.Component {
-//     constructor(props){
-//         super(props);
-//         this.state = {
-//           email: "",
-//           picture: ""
-//         }
-//     }
-//     componentDidMount = () => {
-//        helpers.getCurrentUser().then((response) => {
-//           if (response !== this.state.email) {
-//             this.setState({ picture: response.data.picture, email: response.data.email });
-//           }
-//         });
-//    }
-//     render() {
-//        if(this.props.loggedIn){
-//          return (
-//                 <div className="container">
-//                     {this.props.children}
-//                 </div>
-//         );
-//         } 
-//         else{
-//           return <Redirect to="/" />
-//         }
-//     }
-// }
-// export default Employee;
-
-    // function ScheduleHomePage(){
-  
-    //     const columns = ["First Name", "Last Name", "Position", "Start Time", "End-Time"];
-
-    //     const data = [
-    //      ["Joe1", "Snuffy", "TT", "2021-01-01 06:30:00 PST", "2021-01-01 18:30:00 PST"],
-    //      ["Joe2", "Snuffy", "TT", "2021-01-01 06:30:00 PST", "2021-01-01 18:30:00 PST"],
-    //      ["Joe3", "Snuffy", "TT", "2021-01-01 06:30:00 PST", "2021-01-01 18:30:00 PST"],
-    //      ["Paul", "Asphalt", "MCC", "2021-01-01 06:30:00 PST'", "2021-01-01 18:30:00 PST"],
-    //     ];
-
-    //     const options = {
-    //         filterType: 'checkbox',
-    //       };
-  
-
-    //     return ( 
-    //         <div className="row">
-    //             <header> 
-    //                 <h1> Crew Schedule View </h1>
-    //                     </header>
-    //             <hr />
-    //             <MUIDataTable 
-    //                     data={data} 
-    //                     columns={columns} 
-    //                     options={options} 
-    //                 />
-    //         </div>
-    //     );
-    // }
-
+export default ViewEntireSchedule;
