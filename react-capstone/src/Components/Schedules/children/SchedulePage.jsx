673e0d22e34a356d4b39b353868b6447c1da5e23
dameon <dameon.adolphin@gmail.com>
2021-01-15 08:47:27 -0800

    Made Schedule Daily Display instead of week

--- react-capstone/src/Components/Schedules/children/SchedulePage.jsx
+++ react-capstone/src/Components/Schedules/children/SchedulePage.jsx
@@ -18,7 +18,7 @@ class SchedulePage extends React.Component {
             ScheduleData: [],
             date: new Date(),
             selectedStartDate: Date.now(),
-            selectedEndDate: Date.now()
+            selectedEndDate: Date.now(),
         }
         
     }
@@ -31,22 +31,23 @@ class SchedulePage extends React.Component {
         });  
       } 
       
-      handleSearch = (event) => {
-        this.setState({})
-      }
+      handleDateChange = (date) => {
+        this.setState({ selectedStartDate: date })
+    }
 
-   
+    handleDateChange = (event) => {
+      if(event.target.id === "lastName"){
+        this.setState({date: event.target.id })
+      }
+        
+    }
+
+ 
 
       render() {  
         return (       
           <>
-              <label>
-                  Search Name:
-                    <input type="text" value={this.state.first_name} onChange={this.handleSearch} />
-                </label>
-                    <br />
-                    <br/>
-          <h7><center>Schedule Weekly Page &#160;
+          <h7><center>Schedule Daily Page &#160;
           {this.state.date.toLocaleDateString()}
             </center></h7>
           <TableContainer component={Paper}>  
@@ -55,31 +56,37 @@ class SchedulePage extends React.Component {
               <TableRow>  
               <TableCell>Name</TableCell> 
               <TableCell align="right">&#160; &#160;Position</TableCell> 
-                <TableCell align="right">&#160; &#160;Monday</TableCell>
-                <TableCell align="right">&#160;Tuesday</TableCell> 
+              <TableCell align="right">&#160; &#160;Start Time</TableCell> 
+              <TableCell align="right">&#160; &#160;Stop Time</TableCell> 
+                {/* <TableCell align="right">&#160; &#160;Monday</TableCell> */}
+                {/* <TableCell align="right">&#160;Tuesday</TableCell> 
                 <TableCell align="right">&#160;&#160;Wednesday</TableCell>
                 <TableCell align="right">&#160;Thursday</TableCell>
                 <TableCell align="right">&#160;&#160;Friday</TableCell> 
                 <TableCell align="right">&#160;Saturday</TableCell>
-                <TableCell align="right">Sunday</TableCell>    
+                <TableCell align="right">&#160;&#160;Sunday</TableCell>
+                <TableCell align="right">Monday</TableCell>     */}
                 </TableRow> 
             </TableHead>   
             <TableBody>      
               {  
+              //{this.props.schedules.findIndex(schedule => schedule.start_time === p.start_time)].start_time}
   
                 this.state.ScheduleData.map((p, index) => {  
                   return <TableRow key={index}>  
                    <TableCell component="th" scope="row"> {this.props.users[this.props.users.findIndex(user => user.user_id === p.user_id)].last_name}
                    &#160; {this.props.users[this.props.users.findIndex(user => user.user_id === p.user_id)].first_name}</TableCell> 
                    <TableCell align="right">{this.props.static.positions[this.props.static.positions.findIndex(position => position.position_id === p.position_id)].position_name}</TableCell> 
-                   <TableCell align="right">7am-7pm</TableCell> 
-                   <TableCell  align="right">7pm-7am</TableCell>
+                   <TableCell align="right">{p.start_time}</TableCell> 
+                   <TableCell  align="right">{p.stop_time}</TableCell>
+                   {/* <TableCell align="right">{this.state.selectedStartDate[this.state.selectedStartDate.findIndex(schedule => schedule.start_time === p.date)].date}</TableCell>  */}
+                   {/* <TableCell  align="right">7pm-7am</TableCell>
                     <TableCell align="right">10am-4pm</TableCell> 
                     <TableCell align="right">OFF</TableCell> 
-                    <TableCell align="right">10am-4pm</TableCell> 
+                    <TableCell align="right">OFF</TableCell> 
                     <TableCell align="right">4pm-11pm</TableCell>   
-                    <TableCell align="right">OFF</TableCell>   
-                           
+                    <TableCell align="right">7am-7pm</TableCell>
+                    <TableCell align="right">12pm-12am</TableCell>            */} 
                    </TableRow>   
                 })   
               }  
